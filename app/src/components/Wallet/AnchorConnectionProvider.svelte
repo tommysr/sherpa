01b1568a8db9f422e91eac50086c548c9a1360c9
tommysr <47206288+tommysr@users.noreply.github.com>
2024-03-06 15:23:15 +0100

    import svelte wallet

---
+++ app/src/components/Wallet/AnchorConnectionProvider.svelte
@@ -0,0 +1,42 @@
+<!-- <script lang="ts">
+  import { Connection } from '@solana/web3.js'
+  import type { Commitment, ConnectionConfig } from '@solana/web3.js'
+  import { AnchorProvider, Program } from '@coral-xyz/anchor'
+  import { walletStore, type WalletStore } from '$lib/stores/wallet'
+  import { anchorStore } from '$lib/src/stores/anchor'
+  //import { IDL } from '$src/utils/Idl/protocol'
+  //import { PROGRAM_ID } from '$src/stores/protocolStateStore'
+
+  export let network: string,
+    config: Commitment | ConnectionConfig | undefined = 'processed'
+
+  const connection = new Connection(network, config)
+
+  function defineProgramAndProvider(walletStore: WalletStore) {
+    let { signTransaction, signAllTransactions, publicKey } = walletStore
+
+    const provider = new AnchorProvider(
+      connection,
+      {
+        publicKey: publicKey!,
+        signAllTransactions: signAllTransactions!,
+        signTransaction: signTransaction!
+      },
+      {
+        preflightCommitment: 'processed'
+      }
+    )
+
+    const program = new Program(IDL, PROGRAM_ID, provider)
+
+    anchorStore.set({
+      connection,
+      program,
+      network
+    })
+  }
+
+  $: $walletStore && $walletStore.publicKey && defineProgramAndProvider($walletStore)
+</script> -->
+
+<slot />
---
+++ app/src/components/Wallet/ConnectionProvider.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Connection } from '@solana/web3.js';
+	import type { Commitment, ConnectionConfig } from '@solana/web3.js';
+	import { web3Store } from '$src/stores/web3';
+
+	export let network: string,
+		config: Commitment | ConnectionConfig | undefined = 'processed';
+
+	const connection = new Connection(network, config);
+	web3Store.set({ connection });
+</script>
