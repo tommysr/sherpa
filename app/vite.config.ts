d9774e889c05b545ca75005e2d260c1080324b0d
tommysr <47206288+tommysr@users.noreply.github.com>
2024-03-06 15:22:32 +0100

    add configs, tools

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$src: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
+			$stores: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src', 'stores'),
+			$components: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src', 'components'),
+			$utils: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src', 'utils')
+		}
+	},
+	define: {
+		'process.env.ANCHOR_BROWSER': true
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 });
