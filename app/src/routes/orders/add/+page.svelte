d93f3239dae02eb2ab578d56a5f8997e792859ed
tommysr <47206288+tommysr@users.noreply.github.com>
2024-03-07 19:04:24 +0100

    add new properties to shipment form

--- app/src/routes/orders/add/+page.svelte
+++ app/src/routes/orders/add/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import ShipmentMap from '$components/ShipmentMap/ShipmentMap.svelte';
+	import type { MockTransportOrder } from '$src/utils/types/mockTransport';
 	import Geolocation from 'svelte-geolocation';
 	import type { GeolocationCoords } from 'svelte-geolocation/types/Geolocation.svelte';
 
@@ -14,11 +15,13 @@
 		const coords = e.detail.coords;
 		shipmentSourceCoord = [coords.longitude, coords.latitude];
 	}
+
+	let shipmentOrder: MockTransportOrder;
 </script>
 
 <main class="container">
-	<div class="grid">
-		<div>
+	<div class="grid" id="maingrid">
+		<div id="order">
 			<form>
 				<label for="amount">Ship payment in SOL</label>
 				<input
@@ -37,12 +40,8 @@
 							<td>Source Location </td>
 							<td>
 								<div data-type="amount">
-									<span id="transaction-fees-btc" data-type="btc"
-										>Longitude: {shipmentSourceCoord[0].toFixed(4)}</span
-									>
-									<span id="transaction-fees-usd" data-type="usd"
-										>Latitude: {shipmentSourceCoord[1].toFixed(4)}</span
-									>
+									<span>Longitude: {shipmentSourceCoord[0].toFixed(4)}</span>
+									<span>Latitude: {shipmentSourceCoord[1].toFixed(4)}</span>
 								</div>
 							</td>
 						</tr>
@@ -51,22 +50,113 @@
 							<td>Destination Location</td>
 							<td>
 								<div data-type="amount">
-									<span id="transaction-fees-btc" data-type="btc"
-										>Longitude: {shipmentDestinationCoord[0].toFixed(4)}</span
-									>
-									<span id="transaction-fees-usd" data-type="usd">
-										Latitude: {shipmentDestinationCoord[1].toFixed(4)}</span
-									>
+									<span>Longitude: {shipmentDestinationCoord[0].toFixed(4)}</span>
+									<span> Latitude: {shipmentDestinationCoord[1].toFixed(4)}</span>
 								</div>
 							</td>
 						</tr>
+
+						<tr>
+							<td>Weight</td>
+							<td>
+								<div class="grid">
+									<input
+										type="weight"
+										name="weight"
+										placeholder="weight"
+										aria-label="weight"
+										required
+									/>
+									<select
+										name="favorite-cuisine"
+										aria-label="Select your favorite cuisine..."
+										required
+									>
+										<option selected disabled value=""> kg/lb </option>
+										<option>kg</option>
+										<option>lb</option>
+									</select>
+								</div>
+							</td></tr
+						>
+						<tr>
+							<td>Date</td><td>
+								<input
+									type="datetime-local"
+									name="datetime-local"
+									aria-label="Datetime local"
+									required
+								/></td
+							>
+						</tr>
+						<tr
+							><td colspan="2">
+								<label for="priority">Priority</label>
+								<input
+									id="priority"
+									name="priority"
+									list="priorities"
+									type="range"
+									min="1"
+									max="7"
+									step="1"
+									value="3"
+									required
+								/>
+								<datalist id="priorities">
+									<option value="1">Min</option>
+									<option value="2">Low</option>
+									<option value="3">Medium</option>
+									<option value="4">High</option>
+									<option value="5">Max</option>
+								</datalist>
+							</td></tr
+						>
+						<tr>
+							<td>Fragility</td><td
+								><fieldset>
+									<legend>Describe the package:</legend>
+									<label>
+										<input type="radio" name="fragility" checked />
+										Ice Packaging
+									</label>
+									<label>
+										<input type="radio" name="fragility" />
+										Vacuum-Sealed Packaging
+									</label>
+									<label>
+										<input type="radio" name="fragility" />
+										Glass Containers
+									</label>
+									<label>
+										<input type="radio" name="fragility" />
+										Thin Plastic Wrap
+									</label>
+								</fieldset></td
+							>
+						</tr>
 					</tbody>
+					<tfoot>
+						<tr>
+							<td colspan="2">
+								<fieldset>
+									<legend>Shipment preferences:</legend>
+									<input type="checkbox" id="international" name="international" />
+									<label for="international">International</label>
+									<input type="checkbox" id="tracked" name="tracked" />
+									<label for="tracked">Tracked shipping</label>
+									<input type="checkbox" id="signature" name="signature" />
+									<label for="signature">Signature Required</label>
+								</fieldset></td
+							>
+						</tr>
+					</tfoot>
 				</table>
 
 				<button type="submit">Create</button>
 			</form>
 		</div>
-		<div>
+		<div id="map">
 			<Geolocation getPosition watch on:position={handleBrowserPosition} />
 			<ShipmentMap
 				bind:sourceLocation={shipmentSourceCoord}
@@ -77,4 +167,7 @@
 </main>
 
 <style lang="scss">
+	#maingrid {
+		margin-top: 50px;
+	}
 </style>
