c784ad541cf3ed0e9af3515fe53f887afadbb46d
tommysr <47206288+tommysr@users.noreply.github.com>
2024-03-08 15:25:48 +0100

    handling path aliases by svelte

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,22 +1,33 @@
-import adapter from '@sveltejs/adapter-auto'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-  optimizeDeps: {
-    include: ['@coral-xyz/anchor', '@solana/web3.js', 'buffer']
-  },
+	optimizeDeps: {
+		include: ['@coral-xyz/anchor', '@solana/web3.js', 'buffer']
+	},
 
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter()
-  }
-}
+	kit: {
+		alias: {
+			$src: './src/',
+			'$src/*': './src/*',
+			'$components/*': './src/components/*',
+			$utils: './src/utils',
+			'$utils/*': './src/utils/*',
+			$tests: './tests',
+			'$tests/*': './tests/*',
+			'$stores/*': './src/stores/*',
+			'$actions/*': './src/actions/*'
+		},
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
+};
 
-export default config
+export default config;
