3bb2748b4afb3b29abed50dac10f4f370a875b80
tommysr <47206288+tommysr@users.noreply.github.com>
2024-03-09 16:57:45 +0100

    fetching shipments

--- app/src/routes/api/orders/+server.ts
+++ app/src/routes/api/orders/+server.ts
@@ -1,16 +1,20 @@
+import { anchorStore } from '$src/stores/anchor';
 import type { MockTransportOrder } from '$src/utils/types/mockTransport';
+import { error, json } from '@sveltejs/kit';
 import { generateMockTransportOrders } from './mockUtils';
+import { get } from 'svelte/store';
 
 export async function GET() {
-	// TODO: get data from anchor using anchor store, then parse it and return to client
+	const { program } = get(anchorStore);
 
-	const ordersFetched: MockTransportOrder[] = generateMockTransportOrders(10);
+	let shipments = await program.account.shipment.all();
 
-	const ordersParsed = JSON.stringify(ordersFetched);
+	console.log(shipments);
 
-	return new Response(ordersParsed, {
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
+	if (!shipments) {
+		// status 500 for now, because we want to retry or cache orders
+		throw error(500, 'No shipments found');
+	}
+
+	return json(shipments);
 }
