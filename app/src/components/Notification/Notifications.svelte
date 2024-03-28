a5e83ef1fe55a0421110b59f64cdcd81307abecb
tejks <sarmatys.programmer@gmail.com>
2024-03-28 15:07:50 +0100

    feat: Move the notification to the single component

--- app/src/components/Notification/Notifications.svelte
+++ app/src/components/Notification/Notifications.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import Notification from './Notification.svelte';
 	import { notificationStore } from './notificationsStore';
-
-	export let notificationComponent: ConstructorOfATypedSvelteComponent;
 </script>
 
 <slot />
-<div>
-	<div class="fixed bottom-0 left-0">
-		{#each $notificationStore as notification}
-			<Notification {notification} {notificationComponent} />
-		{/each}
-	</div>
+
+<div class="fixed bottom-0 left-0">
+	{#each $notificationStore as notification}
+		<Notification {notification} />
+	{/each}
 </div>
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-	// CONSIDER: this scss file import some picocss stylesheets and
-	// pico overrides the default styles of some components like button, input, etc.
-	// so, it's ruining the default styles of components like Map
-
 	// sass
 	import '$src/sass/main.scss';
 
@@ -17,13 +13,14 @@
 		TrezorWalletAdapter,
 		TrustWalletAdapter
 	} from '@solana/wallet-adapter-wallets';
+
 	//wallet components
 	import AnchorConnectionProvider from '$src/components/Wallet/AnchorConnectionProvider.svelte';
 	import ConnectionProvider from '$src/components/Wallet/ConnectionProvider.svelte';
 	import WalletProvider from '$src/components/Wallet/WalletProvider.svelte';
+
 	// solana
 	import { clusterApiUrl } from '@solana/web3.js';
-	import CustomNotification from '$src/components/Notification/CustomNotification.svelte';
 	import Notifications from '$src/components/Notification/Notifications.svelte';
 
 	let wallets: Adapter[];
@@ -45,6 +42,6 @@
 <ConnectionProvider {network} />
 <WalletProvider {localStorageKey} {wallets} autoConnect />
 
-<Notifications notificationComponent={CustomNotification}>
+<Notifications>
 	<slot />
 </Notifications>
